e36ff986362cf4934fda0a3b910e1d8791e4783d
Mourjo Sen <mourjo@helpshift.com>
2018-10-24 09:55:05 +0530

    Migrate to IntelliJ Idea from Eclipse

--- src/scratch/ProducerConsumerBlockingQueue.java
+++ src/scratch/ProducerConsumerBlockingQueue.java
@@ -10,89 +10,89 @@ import java.util.concurrent.Future;
 import util.Colors;
 
 class Producer implements Runnable {
-	private ArrayBlockingQueue<Integer> q;
-	private String color;
-	private int delay;
+    private ArrayBlockingQueue<Integer> q;
+    private String color;
+    private int delay;
 
-	Producer(ArrayBlockingQueue<Integer> q, String color, int delay) {
-		this.q = q;
-		this.color = color;
-		this.delay = delay;
-	}
+    Producer(ArrayBlockingQueue<Integer> q, String color, int delay) {
+        this.q = q;
+        this.color = color;
+        this.delay = delay;
+    }
 
-	@Override
-	public void run() {
-		Random r = new Random();
-		for (int i = 0; i < 10; i++) {
-			try {
-				int x = r.nextInt(1000);
-				q.put(x);
-				System.out.println(color + "Produced: " + x);
-				Thread.sleep(r.nextInt(delay));
-			} catch (InterruptedException e) {
-			}
-		}
-		System.out.println(color + "*** Finished producing ***");
-	}
+    @Override
+    public void run() {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            try {
+                int x = r.nextInt(1000);
+                q.put(x);
+                System.out.println(color + "Produced: " + x);
+                Thread.sleep(r.nextInt(delay));
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println(color + "*** Finished producing ***");
+    }
 }
 
 class Consumer implements Runnable {
-	private ArrayBlockingQueue<Integer> q;
-	private String color;
-	private int delay;
+    private ArrayBlockingQueue<Integer> q;
+    private String color;
+    private int delay;
 
-	Consumer(ArrayBlockingQueue<Integer> q, String color, int delay) {
-		this.q = q;
-		this.color = color;
-		this.delay = delay;
-	}
+    Consumer(ArrayBlockingQueue<Integer> q, String color, int delay) {
+        this.q = q;
+        this.color = color;
+        this.delay = delay;
+    }
 
-	@Override
-	public void run() {
-		Random r = new Random();
-		while (true) {
-			try {
-				Thread.sleep(r.nextInt(delay));
+    @Override
+    public void run() {
+        Random r = new Random();
+        while (true) {
+            try {
+                Thread.sleep(r.nextInt(delay));
 
-				int k = q.take();
-				if (k == Integer.MIN_VALUE) {
-					q.put(k);
-					break;
-				} else
-					System.out.println(color + "\t\t\tConsumed: " + k);
+                int k = q.take();
+                if (k == Integer.MIN_VALUE) {
+                    q.put(k);
+                    break;
+                } else
+                    System.out.println(color + "\t\t\tConsumed: " + k);
 
-			} catch (InterruptedException e) {
-			}
-		}
-		System.out.println(color + "\t\t\t***Finished consuming***");
-	}
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println(color + "\t\t\t***Finished consuming***");
+    }
 }
 
 public class ProducerConsumerBlockingQueue {
 
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		
-		ArrayBlockingQueue<Integer> q = new ArrayBlockingQueue<Integer>(3);
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-		ExecutorService e = Executors.newFixedThreadPool(10);
+        ArrayBlockingQueue<Integer> q = new ArrayBlockingQueue<>(3);
 
-		e.submit(new Consumer(q, Colors.nextColor(), 100));
-		e.submit(new Consumer(q, Colors.nextColor(), 200));
-		e.submit(new Consumer(q, Colors.nextColor(), 300));
-		e.submit(new Consumer(q, Colors.nextColor(), 400));
+        ExecutorService e = Executors.newFixedThreadPool(10);
 
-		Future<?> producer1 = e.submit(new Producer(q, Colors.nextColor(), 100));
-		Future<?> producer2 = e.submit(new Producer(q, Colors.nextColor(), 150));
-		Future<?> producer3 = e.submit(new Producer(q, Colors.nextColor(), 200));
+        e.submit(new Consumer(q, Colors.nextColor(), 100));
+        e.submit(new Consumer(q, Colors.nextColor(), 200));
+        e.submit(new Consumer(q, Colors.nextColor(), 300));
+        e.submit(new Consumer(q, Colors.nextColor(), 400));
 
-		e.shutdown();
+        Future<?> producer1 = e.submit(new Producer(q, Colors.nextColor(), 100));
+        Future<?> producer2 = e.submit(new Producer(q, Colors.nextColor(), 150));
+        Future<?> producer3 = e.submit(new Producer(q, Colors.nextColor(), 200));
 
-		producer1.get();
-		producer2.get();
-		producer3.get();
+        e.shutdown();
 
-		q.put(Integer.MIN_VALUE);
+        producer1.get();
+        producer2.get();
+        producer3.get();
 
-		System.out.println(Colors.ANSI_RESET + "Main thread exits...");
-	}
+        q.put(Integer.MIN_VALUE);
+
+        System.out.println(Colors.ANSI_RESET + "Main thread exits...");
+    }
 }
